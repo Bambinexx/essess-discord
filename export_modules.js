a91e59fbff5ca8ec12cd48cadabd64c17a20673c
bambinexx <m.abrias@ndfrance.org>
2022-10-10 12:09:32 +0200

    fix export_modules

--- export_modules.js
+++ export_modules.js
@@ -11,19 +11,19 @@ const otherCommandsPath =  path.join(commandsPath, 'other')
 const commandsFiles = {
     osuCommands: [
         fs.readdirSync(osuCommandsPath).filter(file => file.endsWith(".js")),
-        osuCommandsPath
+        osuCommandsPath + "/"
     ],
     otherCommands: [
         fs.readdirSync(otherCommandsPath).filter(file => file.endsWith(".js")),
-        otherCommandsPath
+        otherCommandsPath + "/"
     ]
 }
 
-const osucommandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
 for (const [k, val] of Object.entries(commandsFiles)) {
     for (const file of val[0]) {
         command = require(val[1] + file)
+        console.log('require work')
         commands.push(command.data.toJSON)
     }
 }
